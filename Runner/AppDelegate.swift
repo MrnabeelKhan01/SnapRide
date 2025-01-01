e775d3d8e7fd1daf7ef521e1be5c69c41404157a
“Nabeel <0000>
2025-01-01 19:32:41 +0500

    Working on Backend

---
+++ Runner/AppDelegate.swift
@@ -0,0 +1,14 @@
+import UIKit
+import Flutter
+import GoogleMaps
+@main
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+     GMSServices.provideAPIKey("AIzaSyDymNHCAZQfbNeM6WiPDNJMxcpNAYXmSmE")
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
